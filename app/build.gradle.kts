da264fa5c6aa2b17257d9e0cc6341ae0c23718e9
Lucas Emboava <emboavalucas@hotmail.com>
2024-02-03 00:54:34 -0300

    3rd-building a details screen from Retrofit response

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,7 @@ dependencies {
     implementation("com.squareup.moshi:moshi:1.12.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+
+    //Picasso
+    implementation("com.squareup.picasso:picasso:2.71828")
 }
--- app/src/main/java/com/emboava/rickmortyapp/MainActivity.kt
+++ app/src/main/java/com/emboava/rickmortyapp/MainActivity.kt
@@ -3,10 +3,12 @@ package com.emboava.rickmortyapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.squareup.picasso.Picasso
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -18,7 +20,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView = findViewById<TextView>(R.id.textView)
+        val nameTextView = findViewById<AppCompatTextView>(R.id.nameTextView)
+        val headerImageView = findViewById<AppCompatImageView>(R.id.headerImageView)
+        val genderImageView = findViewById<AppCompatImageView>(R.id.genderImageView)
+        val aliveTextView = findViewById<AppCompatTextView>(R.id.aliveTextView)
+        val originTextView = findViewById<AppCompatTextView>(R.id.originTextView)
+        val speciesTextView = findViewById<AppCompatTextView>(R.id.speciesTextView)
 
         val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
 
@@ -39,9 +46,20 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 val body = response.body()!!
-                val name = body.name
 
-                textView.text = name
+                nameTextView.text = body.name
+                aliveTextView.text = body.status
+                speciesTextView.text = body.species
+                originTextView.text = body.origin.name
+
+                if (body.gender.equals("male", true)) {
+                    genderImageView.setImageResource(R.drawable.ic_male_24)
+                } else {
+                    genderImageView.setImageResource(R.drawable.ic_female_24)
+                }
+
+                Picasso.get().load(body.image).into(headerImageView)
+
             }
 
             override fun onFailure(call: Call<GetCharacterByIdResponse>, t: Throwable) {
