72b5f5890d165aba6e3d4a2f5f9e45b0029a9b1e
Lucas Emboava <emboavalucas@hotmail.com>
2024-02-04 00:53:09 -0300

    6th: flexible ui with epoxy

--- app/src/main/java/com/emboava/rickmortyapp/MainActivity.kt
+++ app/src/main/java/com/emboava/rickmortyapp/MainActivity.kt
@@ -3,10 +3,8 @@ package com.emboava.rickmortyapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.appcompat.widget.AppCompatImageView
-import androidx.appcompat.widget.AppCompatTextView
 import androidx.lifecycle.ViewModelProvider
-import com.squareup.picasso.Picasso
+import com.airbnb.epoxy.EpoxyRecyclerView
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,39 +12,26 @@ class MainActivity : AppCompatActivity() {
         ViewModelProvider(this)[SharedViewModel::class.java]
     }
 
+    private val epoxyController = CharacterDetailsEpoxyController()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val nameTextView = findViewById<AppCompatTextView>(R.id.nameTextView)
-        val headerImageView = findViewById<AppCompatImageView>(R.id.headerImageView)
-        val genderImageView = findViewById<AppCompatImageView>(R.id.genderImageView)
-        val aliveTextView = findViewById<AppCompatTextView>(R.id.aliveTextView)
-        val originTextView = findViewById<AppCompatTextView>(R.id.originTextView)
-        val speciesTextView = findViewById<AppCompatTextView>(R.id.speciesTextView)
-
-
-        viewModel.refreshCharacter(54)
         viewModel.characterByIdLiveData.observe(this) { response ->
+
+            epoxyController.characterResponse = response
+
             if (response == null) {
                 Toast.makeText(this@MainActivity, "Unsucessful network call!", Toast.LENGTH_LONG)
                     .show()
                 return@observe
             }
-
-            nameTextView.text = response.name
-            aliveTextView.text = response.status
-            speciesTextView.text = response.species
-            originTextView.text = response.origin.name
-
-            if (response.gender.equals("male", true)) {
-                genderImageView.setImageResource(R.drawable.ic_male_24)
-            } else {
-                genderImageView.setImageResource(R.drawable.ic_female_24)
-            }
-
-            Picasso.get().load(response.image).into(headerImageView)
         }
+
+        viewModel.refreshCharacter(54)
+        val epoxyRecyclerView = findViewById<EpoxyRecyclerView>(R.id.epoxyRecyclerView)
+        epoxyRecyclerView.setControllerAndBuildModels(epoxyController)
     }
 }
 
---
+++ app/src/main/java/com/emboava/rickmortyapp/epoxy/LoadingEpoxyModel.kt
@@ -0,0 +1,11 @@
+package com.emboava.rickmortyapp.epoxy
+
+import com.emboava.rickmortyapp.R
+import com.emboava.rickmortyapp.databinding.ModelLoadingBinding
+
+class LoadingEpoxyModel : ViewBindingKotlinModel<ModelLoadingBinding>(R.layout.model_loading) {
+
+    override fun ModelLoadingBinding.bind() {
+        // nothing to do
+    }
+}
