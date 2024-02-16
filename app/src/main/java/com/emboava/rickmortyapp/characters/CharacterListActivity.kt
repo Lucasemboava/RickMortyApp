9e3ea91635231a2e8c4ab51f26ab05d04ba9534e
Lucas Emboava <emboavalucas@hotmail.com>
2024-02-16 13:20:05 -0300

    9th: activity navigation with intents and actionbar

--- app/src/main/java/com/emboava/rickmortyapp/characters/CharacterListActivity.kt
+++ app/src/main/java/com/emboava/rickmortyapp/characters/CharacterListActivity.kt
@@ -1,14 +1,17 @@
 package com.emboava.rickmortyapp.characters
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.airbnb.epoxy.EpoxyRecyclerView
+import com.emboava.rickmortyapp.Constants.INTENT_EXTRA_CHARACTER_ID
+import com.emboava.rickmortyapp.CharacterDetailActivity
 import com.emboava.rickmortyapp.R
 
 class CharacterListActivity: AppCompatActivity() {
 
-    private val epoxyController = CharacterListPagingEpoxyController()
+    private val epoxyController = CharacterListPagingEpoxyController(::onCharacterSelected)
 
     private val viewModel: CharactersViewModel by lazy {
         ViewModelProvider(this).get(CharactersViewModel::class.java)
@@ -23,5 +26,11 @@ class CharacterListActivity: AppCompatActivity() {
         }
 
         findViewById<EpoxyRecyclerView>(R.id.epoxyRecyclerView).setController(epoxyController)
+    }
+
+    private fun onCharacterSelected(characterId: Int) {
+        val intent = Intent(this, CharacterDetailActivity::class.java)
+        intent.putExtra(INTENT_EXTRA_CHARACTER_ID, characterId)
+        startActivity(intent)
     }
 }
