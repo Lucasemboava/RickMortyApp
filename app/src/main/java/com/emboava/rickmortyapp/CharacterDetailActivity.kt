2cf9969ca933228b41974a054e3b4302b4df0ac6
Lucas Emboava <emboavalucas@hotmail.com>
2024-02-16 13:20:05 -0300

    9th: activity navigation with intents and actionbar

--- app/src/main/java/com/emboava/rickmortyapp/CharacterDetailActivity.kt
+++ app/src/main/java/com/emboava/rickmortyapp/CharacterDetailActivity.kt
@@ -2,11 +2,13 @@ package com.emboava.rickmortyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.airbnb.epoxy.EpoxyRecyclerView
+import com.emboava.rickmortyapp.Constants.INTENT_EXTRA_CHARACTER_ID
 
-class MainActivity : AppCompatActivity() {
+class CharacterDetailActivity : AppCompatActivity() {
 
     private val viewModel: SharedViewModel by lazy {
         ViewModelProvider(this)[SharedViewModel::class.java]
@@ -18,20 +20,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         viewModel.characterByIdLiveData.observe(this) { response ->
 
             epoxyController.characterResponse = response
 
             if (response == null) {
-                Toast.makeText(this@MainActivity, "Unsucessful network call!", Toast.LENGTH_LONG)
+                Toast.makeText(this@CharacterDetailActivity, "Unsucessful network call!", Toast.LENGTH_LONG)
                     .show()
                 return@observe
             }
         }
 
-        viewModel.refreshCharacter(54)
+        val id = intent.getIntExtra(INTENT_EXTRA_CHARACTER_ID, 1)
+        viewModel.refreshCharacter(characterId = id)
         val epoxyRecyclerView = findViewById<EpoxyRecyclerView>(R.id.epoxyRecyclerView)
         epoxyRecyclerView.setControllerAndBuildModels(epoxyController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
 
