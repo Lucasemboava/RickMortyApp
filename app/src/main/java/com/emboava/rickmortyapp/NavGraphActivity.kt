ed3549187f5ccda13c2e969abef83021b3b60a7c
Lucas Emboava <emboavalucas@hotmail.com>
2024-02-26 16:49:44 -0300

    16th: migrating Activities to Fragments

--- app/src/main/java/com/emboava/rickmortyapp/NavGraphActivity.kt
+++ app/src/main/java/com/emboava/rickmortyapp/NavGraphActivity.kt
@@ -2,12 +2,9 @@ package com.emboava.rickmortyapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 
 class NavGraphActivity: AppCompatActivity() {
-
-    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,10 +12,6 @@ class NavGraphActivity: AppCompatActivity() {
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
+        val navController = navHostFragment.navController
     }
-
-
-
-
 }
