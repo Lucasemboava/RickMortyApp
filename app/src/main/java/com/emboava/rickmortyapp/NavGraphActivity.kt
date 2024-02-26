eb1f8a79b49444dc47db30bec18a1e9a8f756068
Lucas Emboava <emboavalucas@hotmail.com>
2024-02-26 19:34:11 -0300

    17th: back navigation with navigation component and activities clean up

--- app/src/main/java/com/emboava/rickmortyapp/NavGraphActivity.kt
+++ app/src/main/java/com/emboava/rickmortyapp/NavGraphActivity.kt
@@ -2,9 +2,15 @@ package com.emboava.rickmortyapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class NavGraphActivity: AppCompatActivity() {
+
+    private  lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,5 +19,16 @@ class NavGraphActivity: AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(
+            navController = navController,
+            configuration = AppBarConfiguration(navController.graph)
+        )
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(this, R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
